05f014c2845318005e58514b433bc77b98de4eb1
Shahzad Ahamad P <shahzadahamadp@gmail.com>
2024-09-28 14:19:16 +0530

    frontend admin interceptor used with admin api client

--- Frontend/src/apis/apiClient.ts
+++ Frontend/src/apis/apiClient.ts
@@ -4,18 +4,29 @@ import {
   requestInterceptor,
   responseInterceptor,
 } from "./interceptors/interceptors";
+import {
+  adminErrorInterceptor,
+  adminRequestInterceptor,
+  adminResponseInterceptor,
+} from "./interceptors/adminInterceptors";
 
 const apiClient: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
- 
-export const adminApiClient: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_ADMIN_BASE_URL,
-  withCredentials: true
-}) 
 
 apiClient.interceptors.request.use(requestInterceptor);
 apiClient.interceptors.response.use(responseInterceptor, errorInterceptor);
+
+export const adminApiClient: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_ADMIN_BASE_URL,
+  withCredentials: true,
+});
+
+adminApiClient.interceptors.request.use(adminRequestInterceptor);
+adminApiClient.interceptors.response.use(
+  adminResponseInterceptor,
+  adminErrorInterceptor
+);
 
 export default apiClient;
