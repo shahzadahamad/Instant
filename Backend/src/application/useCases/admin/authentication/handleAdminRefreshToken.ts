cb92a936c7b824f84bcf8024ceaae4b444981320
Shahzad Ahamad P <shahzadahamadp@gmail.com>
2024-09-28 16:39:23 +0530

    backed code some clearup

--- Backend/src/application/useCases/admin/authentication/handleAdminRefreshToken.ts
+++ Backend/src/application/useCases/admin/authentication/handleAdminRefreshToken.ts
@@ -15,29 +15,19 @@ export default class HandleAdminRefreshToken {
     if (!adminRefreshToken) {
       throw new Error("No refresh token");
     }
-
-    console.log('inin 1')
-
     try {
       const decoded = (await this.tokenManager.verifyRefreshToken(
         adminRefreshToken
       )) as { userId: string };
       const admin = await this.adminRepository.findById(decoded.userId);
-
-      console.log('inin2')
-
       if (!admin) {
         throw new Error("admin not found");
       }
-
-      console.log('inin3')
-
       const newAdminAccessToken = await this.tokenManager.generateAccessToken({
         userId: admin._id,
         role: "admin",
       });
 
-      console.log('new token' + "incresase")
       return { adminToken: newAdminAccessToken };
     } catch (error) {
       return { clearCookie: true };
