ade820feadce3d1cb354176719cbb1d342807182
Shahzad Ahamad P <shahzadahamadp@gmail.com>
2024-10-04 10:19:38 +0530

    strictmode removed

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -10,13 +9,11 @@ import { NextUIProvider } from "@nextui-org/react";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <StrictMode>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <NextUIProvider>
-          <App />
-          <Toaster />
-        </NextUIProvider>
-      </ThemeProvider>
-    </StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <NextUIProvider>
+        <App />
+        <Toaster />
+      </NextUIProvider>
+    </ThemeProvider>
   </Provider>
 );
