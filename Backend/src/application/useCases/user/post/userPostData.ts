afcd14aae1a41b0b4e1d00fd61f116610da90c35
Shahzad Ahamad P <shahzadahamadp@gmail.com>
2024-10-15 23:15:18 +0530

    fix(api): modify logic for Get Post User Data route in server

--- Backend/src/application/useCases/user/post/userPostData.ts
+++ Backend/src/application/useCases/user/post/userPostData.ts
@@ -1,14 +1,25 @@
 import { IPost } from "../../../../infrastructure/database/models/postModel";
 import PostRepository from "../../../repositories/user/postRepository";
+import UserRepository from "../../../repositories/user/userRepository";
 
 export default class UserPostData {
   private postRepository: PostRepository;
+  private userRepository: UserRepository;
 
-  constructor(postRepository: PostRepository) {
+  constructor(postRepository: PostRepository, userRepository: UserRepository) {
     this.postRepository = postRepository;
+    this.userRepository = userRepository;
   }
 
-  public async execute(id: string): Promise<IPost[]> {
+  public async execute(id: string, username: string): Promise<IPost[]> {
+    if (username) {
+      const user = await this.userRepository.findByUsername(username);
+      if (user) {
+        const postData = await this.postRepository.findUserPostData(user._id);
+        return postData;
+      }
+    }
+
     const postData = await this.postRepository.findUserPostData(id);
     return postData;
   }
--- Backend/src/infrastructure/controllers/user/post/getUserPostDataController.ts
+++ Backend/src/infrastructure/controllers/user/post/getUserPostDataController.ts
@@ -1,15 +1,20 @@
 import { Request, Response } from "express";
 import PostRepository from "../../../../application/repositories/user/postRepository";
 import UserPostData from "../../../../application/useCases/user/post/userPostData";
+import UserRepository from "../../../../application/repositories/user/userRepository";
 
 export default class GetUserPostDataController {
   public async handle(req: any, res: Response): Promise<Response | void> {
     const { userId } = req.user;
+    const { username = "" } = req.query;
 
-    const userPostData = new UserPostData(new PostRepository());
+    const userPostData = new UserPostData(
+      new PostRepository(),
+      new UserRepository()
+    );
 
     try {
-      const data = await userPostData.execute(userId);
+      const data = await userPostData.execute(userId, username);
       return res.status(200).json(data);
     } catch (error) {
       if (error instanceof Error) {
