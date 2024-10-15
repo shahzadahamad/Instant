a80dfd3b5482c3e7d355aa573a174e50cdc1147e
Shahzad Ahamad P <shahzadahamadp@gmail.com>
2024-10-15 16:03:27 +0530

    fix: bug fixed while delete post

--- Backend/src/application/useCases/user/post/deletePost.ts
+++ Backend/src/application/useCases/user/post/deletePost.ts
@@ -1,24 +1,34 @@
 import AwsS3Storage from "../../../providers/awsS3Storage";
 import LikeRepository from "../../../repositories/user/likeRepository";
 import PostRepository from "../../../repositories/user/postRepository";
+import UserRepository from "../../../repositories/user/userRepository";
 
 export default class DeletePost {
   private postRepository: PostRepository;
   private awsS3Storage: AwsS3Storage;
   private likeRepository: LikeRepository;
+  private userRepository: UserRepository;
 
   constructor(
     postRepository: PostRepository,
     awsS3Storage: AwsS3Storage,
-    likeRepository: LikeRepository
+    likeRepository: LikeRepository,
+    userRepository: UserRepository
   ) {
     this.postRepository = postRepository;
     this.awsS3Storage = awsS3Storage;
     this.likeRepository = likeRepository;
+    this.userRepository = userRepository;
   }
 
-  public async execute(id: string): Promise<string> {
+  public async execute(id: string, userId: string): Promise<string> {
     const postData = await this.postRepository.findPostById(id);
+
+    const userData = await this.userRepository.findById(userId);
+
+    if (userData?._id.toString() !== postData?.userId.toString()) {
+      throw new Error("Invalid Action!");
+    }
 
     if (!postData) {
       throw new Error("Post not found!");
--- Backend/src/infrastructure/controllers/user/post/deletePostController.ts
+++ Backend/src/infrastructure/controllers/user/post/deletePostController.ts
@@ -3,19 +3,22 @@ import PostRepository from "../../../../application/repositories/user/postReposi
 import DeletePost from "../../../../application/useCases/user/post/deletePost";
 import AwsS3Storage from "../../../../application/providers/awsS3Storage";
 import LikeRepository from "../../../../application/repositories/user/likeRepository";
+import UserRepository from "../../../../application/repositories/user/userRepository";
 
 export default class DeletePostController {
-  public async handle(req: Request, res: Response): Promise<Response | void> {
+  public async handle(req: any, res: Response): Promise<Response | void> {
     const { postId } = req.params;
+    const { userId } = req.user;
 
     const deletePost = new DeletePost(
       new PostRepository(),
       new AwsS3Storage(),
-      new LikeRepository()
+      new LikeRepository(),
+      new UserRepository(),
     );
 
     try {
-      const data = await deletePost.execute(postId);
+      const data = await deletePost.execute(postId, userId);
       return res.status(200).json(data);
     } catch (error) {
       if (error instanceof Error) {
