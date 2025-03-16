3ba0e78142908eb5e6f2642c0d57dc3e09472435
Shahzad Ahamad P <shahzadahamadp@gmail.com>
2025-03-16 11:53:44 +0530

    refactor:(frontend) socket url to .env file

--- Frontend/src/socket/socket.ts
+++ Frontend/src/socket/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
+const URL = import.meta.env.VITE_SOCKET_URL!;
 
 export const socket = io(URL, {
   auth: { token: localStorage.getItem("token") },
