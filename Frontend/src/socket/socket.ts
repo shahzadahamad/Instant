9a6c3f9f8c8a5eb2034b7c0268939358f7be2cdc
Shahzad Ahamad P <shahzadahamadp@gmail.com>
2025-03-16 12:38:23 +0530

    modified the app logo in tab

--- Frontend/src/socket/socket.ts
+++ Frontend/src/socket/socket.ts
@@ -2,6 +2,8 @@ import { io } from 'socket.io-client';
 
 const URL = import.meta.env.VITE_SOCKET_URL!;
 
+console.log(URL);
+
 export const socket = io(URL, {
   auth: { token: localStorage.getItem("token") },
   reconnection: true,
